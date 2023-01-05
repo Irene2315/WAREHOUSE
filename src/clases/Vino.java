685efe8b3a2a0de6e1b40948947e20692119a907
Irene <Irene@LAPTOP-D8COSLF6>
2023-01-05 18:08:18 +0100

    clase vino completa

--- src/clases/Vino.java
+++ src/clases/Vino.java
@@ -1,5 +1,65 @@
 package clases;
 
 public class Vino {
+	
+	private String color;
+	private String origen;
+	private int anio;
+	private String tipoDeUva;
+	private double gradosAlcohol;
+	
+	public Vino(String color, String origen, int anio, String tipoDeUva, double gradosAlcohol) {
+		super();
+		this.color = color;
+		this.origen = origen;
+		this.anio = anio;
+		this.tipoDeUva = tipoDeUva;
+		this.gradosAlcohol = gradosAlcohol;
+	}
+	
+	public Vino() {
+		
+	}
 
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public String getTipoDeUva() {
+		return tipoDeUva;
+	}
+
+	public void setTipoDeUva(String tipoDeUva) {
+		this.tipoDeUva = tipoDeUva;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+	
+	
 }
