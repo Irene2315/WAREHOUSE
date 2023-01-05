e7ccd364cc6a27adfd1683817b6526f474a60e0f
Irene <Irene@LAPTOP-D8COSLF6>
2023-01-05 18:04:13 +0100

    crear clases y completar refresco

---
+++ src/clases/Articulo.java
@@ -0,0 +1,93 @@
+package clases;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Articulo {
+	private static Scanner Scanner;
+	private String code;
+	private String name;
+	private int capacidadBotella;
+	private double precio;
+	private int stock;
+	
+	
+	public Articulo(String code, String name, int capacidadBotella, double precio, int stock) {
+		this.code = code;
+		this.name = name;
+		this.capacidadBotella = capacidadBotella;
+		this.precio = precio;
+		this.stock = stock;
+	}
+	
+	public Articulo() {
+		
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCapacidadBotella() {
+		return capacidadBotella;
+	}
+
+	public void setCapacidadBotella(int capacidadBotella) {
+		this.capacidadBotella = capacidadBotella;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	public void incrementarStock(int cantidad) {
+		int total;
+		total=0;
+		total=total+cantidad;
+		System.out.println("El stock actual es de "+total+ " unidades");
+	}
+	
+	public void disminuirStock(int cantidad) {
+		int total;
+		total=0;
+		total=total-cantidad;
+		System.out.println("El stock actual es de "+total+" unidades");
+	}
+	public void visualizarArticulo(ArrayList <Articulo> articulos, String code_user  ) {
+		
+		for (int i = 0; i < articulos.size(); i++) {
+			
+			if(code == code_user) {
+				System.out.println(articulos.get(i));
+			}
+		}
+	}
+	
+	
+	
+	
+}
---
+++ src/clases/Refresco.java
@@ -0,0 +1,48 @@
+package clases;
+
+public class Refresco {
+ private String sabor;
+ private boolean zumo;
+ private int cantidadAzucar;
+public Refresco(String sabor, boolean zumo, int cantidadAzucar) {
+	super();
+	this.sabor = sabor;
+	this.zumo = zumo;
+	this.cantidadAzucar = cantidadAzucar;
+}
+
+public Refresco() {
+	
+}
+
+public String getSabor() {
+	return sabor;
+}
+
+public void setSabor(String sabor) {
+	this.sabor = sabor;
+}
+
+public boolean getZumo() {
+	return zumo;
+}
+
+public void setZumo(boolean zumo) {
+	this.zumo = zumo;
+}
+
+public int getCantidadAzucar() {
+	return cantidadAzucar;
+}
+
+public void setCantidadAzucar(int cantidadAzucar) {
+	this.cantidadAzucar = cantidadAzucar;
+}
+
+public void visualizarPropiedades(Refresco refresco) {
+System.out.println(" Sabor: " +this.getSabor() + " Zumo: "+this.getZumo() +
+" Cantidad azucar" + this.getCantidadAzucar());
+}
+
+ 
+}
