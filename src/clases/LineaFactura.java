049b593ca0ebc49f364ab9e008c4334ca15c504f
Irene <Irene@LAPTOP-D8COSLF6>
2023-01-06 13:07:55 +0100

    línea factura actualizada

--- src/clases/LineaFactura.java
+++ src/clases/LineaFactura.java
@@ -1,5 +1,51 @@
 package clases;
 
 public class LineaFactura {
+	private int numero;
+	private Articulo articulo;
+	private double cantidad;
+	
+	public LineaFactura(int numero, Articulo articulo, double cantidad) {
+		super();
+		this.numero = numero;
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+	}
+	
+	public LineaFactura() {
+		
+	}
 
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Articulo getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+    public double precioTotal(Articulo articulo) {
+		double total;
+		total=0;
+		total= articulo.getPrecio()*getCantidad();
+		
+		return total;
+	}
+	
 }
