59889ca6c55da86202424286e8f3df144ac1b70f
Irene <Irene@LAPTOP-D8COSLF6>
2023-01-06 17:02:10 +0100

    preparar el ejecutor y el programa principal

--- src/gestor/GestorAlmacenApp.java
+++ src/gestor/GestorAlmacenApp.java
@@ -1,5 +1,14 @@
 package gestor;
 
-public class GestorAlmacenApp {
 
+
+import principal.AlmacenApp;
+
+
+
+public class GestorAlmacenApp {
+	public static void main(String[] args)  {
+		
+		AlmacenApp.run();
+	}
 }
--- src/principal/AlmacenApp.java
+++ src/principal/AlmacenApp.java
@@ -1,10 +1,13 @@
 package principal;
 
+
+
 public class AlmacenApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void run() {
+		
+		
 	}
 
 }
